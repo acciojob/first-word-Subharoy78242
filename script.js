b05375a1e75424a7dc885fe83f038d354ec9ee0b
Subharoy78242 <57645302+Subharoy78242@users.noreply.github.com>
2024-06-27 11:08:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstWord(s) {
-  // your code here
+  // Trim any leading spaces
+  s = s.trim();
+  
+  // Find the first space in the string
+  let firstSpaceIndex = s.indexOf(' ');
+
+  // If there's no space, return the entire string
+  if (firstSpaceIndex === -1) {
+    return s;
+  }
+
+  // Return the substring from the start to the first space
+  return s.substring(0, firstSpaceIndex);
 }
 
 // Do not change the code below
